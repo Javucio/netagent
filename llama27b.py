31c69331053d40af3fb95a50ecdba37fcf9172da
Joiku <joiku@8bitlair.com>
2025-06-07 22:40:31 +0200

    minor fixes to llama27b.py

--- llama27b.py
+++ llama27b.py
@@ -6,7 +6,7 @@ model_name = "meta-llama/Llama-2-7b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_8bit=True)
 
-prompt = "Hola,voy a conectar una api de fortigate a este modelo. Crees que es muy complicado?"
+prompt = "Hola,voy a conectar una api de fortigate a este modelo."
 
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 outputs = model.generate(**inputs, max_length=1500)
